2a8964c982fc1c5361b50dcac409e2eebf37b1a5
raju <rajuchavan14599@gmail.com>
2024-01-11 12:49:42 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,102 @@
+const BASE_URL = "https://open.er-api.com/v6/latest";
+// const BASE_URl =  "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
+
+const dropdowns = document.querySelectorAll(".countries__container select");
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".countries__selector__from select");
+const toCurr = document.querySelector(".countries__selector__to select");
+const msg = document.querySelector(".msg");
+
+
+for (let select of dropdowns) {
+  for (currCode in countryList) {
+    let newOption = document.createElement("option");
+    newOption.innerText = currCode;
+    newOption.value = currCode;
+    if (select.name === "from" && currCode === "USD") {
+      newOption.selected = "selected";
+    } else if (select.name === "to" && currCode === "INR") {
+      newOption.selected = "selected";
+    }
+    select.append(newOption);
+  }
+  select.addEventListener("change", (evt) => {
+    console.log(evt.target.value);
+    updateFlag(evt.target);
+  });
+}
+
+const updateExchangeRate = async () => {
+  let amount = document.querySelector(".input__container input");
+  let amtVal = amount.value;
+
+  console.log(amtVal);
+ 
+  if (amtVal === "" || amtVal < 1) {
+    amtVal = 1;
+    // amount.value = "1";
+  }
+  // const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
+
+  try {
+    let fromVal = fromCurr.value;
+  
+    const URL =  `${BASE_URL}/${fromVal}`;
+
+    let response = await fetch(URL);
+
+    let data = await response.json();
+
+    const rates1 = data.rates;  //copy of currency object related to 'from coutries'
+    console.log(rates1);
+
+    const toVal = toCurr.value;  //selected currency code from 'to countries'
+  
+    console.log(rates1[fromVal]); 
+    console.log(rates1[toVal]);  //obtaining exchange rate for right(to country) according to left(from country) value 1;
+
+    let fromCal = amtVal * rates1[fromVal]
+    let toCal = amtVal * rates1[toVal]
+
+    msg.innerText = `${fromCal} [${fromVal}] = ${parseFloat(toCal.toFixed(2))} [${toVal}]`;
+    // msg.innerText = `${amtVal}  ${rates1[fromVal]} = ${amtVal} ${rates1[toVal]}`;
+  } catch (error) {
+    console.log(error)
+  }
+
+  // let rate = data[toCurr.value.toLowerCase()];
+
+  // let finalAmount = amtVal ;
+  // msg.innerText = `${amtVal} ${rates1[fromVal]} = ${amtVal} ${rates1[toVal]}`;
+  // msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
+};
+
+
+const updateFlag = (element) => {
+  let currCode = element.value;
+  let countryCode = countryList[currCode];
+  let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+  let img = element.parentElement.querySelector("img");
+  img.src = newSrc;
+};
+
+btn.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  updateExchangeRate();
+});
+
+window.addEventListener("load", () => {
+  updateExchangeRate();
+});
+
+// let currencyFetcher = async () => {
+//   try {
+//     let data = await fetch(BASE_URl);
+//     let final = await data.json();
+//     console.log(final.rates.USD)
+//   } catch (error) {
+//     console.log(error)
+//   }
+// }
+
+// currencyFetcher();
